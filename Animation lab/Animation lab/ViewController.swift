6c0b7c3a2f3caec7bb89acb58d79b97d2c46d010
Aziz <Aziz.programmer1@gmail.com>
2021-12-06 18:25:22 +0300

    Animation

---
+++ Animation lab/Animation lab/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  Animation lab
+//
+//  Created by Abdulaziz on 02/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let boomButton = UIButton(type: .system)
+    var boomEmoji = UILabel()
+    var yConstraint: NSLayoutConstraint?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+        view.backgroundColor = .systemGray6
+        setupBoomButton()
+        setupBoomLabel()
+    }
+
+    func setupBoomButton(){
+        view.addSubview(boomButton)
+        boomButton.setImage(UIImage(systemName: "projective"), for: .normal)
+        boomButton.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            boomButton.heightAnchor.constraint(equalToConstant: 70),
+            boomButton.widthAnchor.constraint(equalToConstant: 70),
+            boomButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            boomButton.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -60)
+        ])
+        boomButton.addTarget(self, action: #selector(boom), for: .touchDown)
+        
+    }
+    
+    func setupBoomLabel(){
+        view.addSubview(boomEmoji)
+        boomEmoji.translatesAutoresizingMaskIntoConstraints = false
+        boomEmoji.text = "🚀"
+        boomEmoji.textAlignment = .center
+        boomEmoji.font = .systemFont(ofSize: 60)
+        NSLayoutConstraint.activate([
+            boomEmoji.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            boomEmoji.heightAnchor.constraint(equalToConstant: 90),
+            boomEmoji.widthAnchor.constraint(equalToConstant: 90),
+            boomEmoji.bottomAnchor.constraint(equalTo: boomButton.topAnchor, constant: 20)
+        ])
+        yConstraint = boomEmoji.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 250)
+        yConstraint?.isActive = true
+    }
+    
+    @objc func boom(){
+        UIView.animate(withDuration: 0.7,
+                       delay: 0.6,
+                       options: .curveLinear) {
+            self.yConstraint?.constant = -350
+            self.view.layoutIfNeeded()
+        } completion: { _ in
+            self.boomEmoji.text = "🔥"
+        }
+
+    }
+}
+
+
+
